15dda8825652fca26371f0ef47cee0cfd9617cd9
Niranjan BR <niranjankannan2003@gmail.com>
2024-11-06 17:23:51 +0000

    ScriptSculptor: Move cloning resource to different script
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- rom.sh
+++ rom.sh
@@ -19,30 +19,3 @@ repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-# Clone device sources
-
-# device tree
-git clone https://github.com/narikootam-dev/device_xiaomi_sweet device/xiaomi/sweet 
-
-# vendor tree
-git clone https://github.com/narikootam-dev/vendor_xiaomi_sweet vendor/xiaomi/sweet 
-
-# kernel tree
-git clone https://github.com/narikootam-dev/kernel_xiaomi_msm4.14 kernel/xiaomi/sweet
-
-# clone extra repos
-# miui camera
-git clone https://gitlab.com/mrfox2003/sweet-miuicamera vendor/xiaomi/sweet-miuicamera
-
-# dolby
-git clone https://github.com/narikootam-dev/hardware_dolby hardware/dolby
-
-# hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
-
-# Lets build it!
-
-. build/envsetup.sh
-
-brunch sweet
- 
